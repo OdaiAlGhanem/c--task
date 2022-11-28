19945d256417551d753a189523e080ea6ab6432b
OdaiAlGhanem <88395007+OdaiAlGhanem@users.noreply.github.com>
2022-11-28 16:39:26 +0300

    update

--- TestApp1/28-11 oop/Program.cs
+++ TestApp1/28-11 oop/Program.cs
@@ -1,19 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
-using _28_11_oop2.odai;
+using System.Xml.Linq;
 
 namespace _28_11_oop
 {
+    public class Car 
+    {
+        public int year;
+        public string Type;
+        public int Price;
+        public string model;
+        public int Palletnumber;
+        public string color;
+
+        public void start()
+        {
+            Console.WriteLine("Do you want to start the engin? yes/no");
+
+            string start = Console.ReadLine().ToLower();
+            if (start == "yes")
+            {
+
+                Console.WriteLine("Engine is ON ");
+
+            }
+            else if (start == "no")
+            {
+                Console.WriteLine("Engine is OFF");
+            }
+        }
+
+    }
+    public class bmw : Car 
+    {
+        public bmw(int CarYear, string CarType, int CarPrice, string CarModel, int CarPalletnumber, string CarColor)
+        {
+            year = CarYear;
+            Type = CarType;
+            Price = CarPrice;
+            model = CarModel;
+            Palletnumber = CarPalletnumber;
+            color = CarColor;
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            _28_11_oop2.Class1 p = null;
-            Class2 p2 = null;
+            Car odai = new bmw(2020, "bmw", 15000, "M5", 300001, "black");
+            
+            Console.WriteLine("Car year :" + odai.year + "\n" + "Car type :" + odai.Type + "\n" + "Car price :" + odai.Price + "\n" + "Car model :" + odai.model + "\n" + "Car pattelnumber :" + odai.Palletnumber + "\n" + "Car color :" + odai.color);
+           odai.start(); 
+        
         }
     }
-
 }
